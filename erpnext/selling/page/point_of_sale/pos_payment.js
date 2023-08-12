e80ef3234dc09e19f03c168fdfac48884ab50eff
Long Nguyen <long.nguyen2@coxautoinc.com>
2023-08-12 11:49:53 -0500

    Allow zero payment

--- erpnext/selling/page/point_of_sale/pos_payment.js
+++ erpnext/selling/page/point_of_sale/pos_payment.js
@@ -203,7 +203,7 @@ erpnext.PointOfSale.Payment = class {
 			const paid_amount = doc.paid_amount;
 			const items = doc.items;
 
-			if (paid_amount == 0 || !items.length) {
+			if (!items.length) {
 				const message = items.length ? __("You cannot submit the order without payment.") : __("You cannot submit empty order.");
 				frappe.show_alert({ message, indicator: "orange" });
 				frappe.utils.play_sound("error");
